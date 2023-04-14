71c474c3b2375fa873556fa908d4568b520a2f59
DNS <DNS@DESKTOP-UGIN9D2>
2023-04-14 20:59:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	cout << "Задание 2\n";
+	cout << "Каждый\n";
+	cout << "\tОхотник\n";
+	cout << "\t\tЖелает\n";
+	cout << "\t\t\tЗнать\n";
+	cout << "\t\t\t\tГде\n";
+	cout << "\t\t\t\t\tСидит\n";
+	cout << "\t\t\t\t\t\tФазан\n\n";
+
+	cout << "Задание 1\n";
+	cout << "I wear women's underwear\n";
+	cout << "And then I go to strike a pose in my full length mirror\n";
+	cout << "I cross my legs just like a queer\n";
+	cout << "But my libido is strong when a lady is near, ya\n";
+	cout << "What defines a straight man's straight?\n";
+	cout << "Is it the boxer in the briefs or a 12 ounce steak?\n";
+	cout << "I tell you what a women loves most\n";
+	cout << "It's a man who can slap but can also stroke\n";
+	cout << "\tVerbatim\n";
+	cout << "\tMother Mother\n";
+
+	return 0;
+}
